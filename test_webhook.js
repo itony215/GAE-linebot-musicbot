7d4bbbcddec45a90169601da28592fcc69625e26
tonywang <tonywang@iii.org.tw>
2017-05-26 17:54:43 +0800

    local test bot

---
+++ test_webhook.js
@@ -0,0 +1,65 @@
+'use strict'
+const line = require('./index')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+// need raw buffer for signature validation
+app.use(bodyParser.json({
+  verify (req, res, buf) {
+    req.rawBody = buf
+  }
+}))
+
+// init with auth
+line.init({
+  accessToken: 'c/XMmaPokDjHzMTNmPed0Mjtf3UZ8S/9+tTB08iIELrmaP5vydkuPLQVPMat1cfVV4H4dfFph4sc1S91OSOg8PWmSg1JbGIyXP7WJvZ1e2X3LJ0zCdKQsk4OS0QkCzlvVY3GqF8UOPa6hGJixR99KgdB04t89/1O/w1cDnyilFU=',
+  // (Optional) for webhook signature validation
+  channelSecret: '22fc220ce651793e6b4a546b028a230a'
+})
+
+/**
+ * response example (https://devdocs.line.me/ja/#webhook):
+ * {
+ *   "events": [
+ *     {
+ *       "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+ *       "type": "message",
+ *       "timestamp": 1462629479859,
+ *       "source": {
+ *         "type": "user",
+ *         "userId": "u206d25c2ea6bd87c17655609a1c37cb8"
+ *       },
+ *       "message": {
+ *         "id": "325708",
+ *         "type": "text",
+ *         "text": "Hello, world"
+ *       }
+ *     }
+ *   ]
+ * }
+ */
+
+app.post('/webhook/', line.validator.validateSignature(), (req, res, next) => {
+  // get content from request body
+  const promises = req.body.events.map(event => {
+    // reply message
+    return line.client
+      .replyMessage({
+        replyToken: event.replyToken,
+        messages: [
+          {
+            type: 'text',
+            text: event.message.text
+          }
+        ]
+      })
+  })
+  Promise
+    .all(promises)
+    .then(() => res.json({success: true}))
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Example app listening on port 3000!')
+})
