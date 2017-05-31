e0dab7f905f3ee028213d10cb05726f619b4b66d
Tony Wang <tonywang@TonydeMacBook-Pro-2.local>
2017-05-31 14:54:34 +0800

    cron update

--- test/index.js
+++ test/index.js
@@ -1,2 +1,12 @@
 var push = require('./push.js');
+var express = require('express');
+const app = express();
+var server = app.listen(process.env.PORT || 8081, function(){
+  var port = server.address().port;
+  console.log('App now running on port',port);
+})
+app.get('/push', (req, res) => {
+  res.status(200).send('Hello, world!');
+  push.push('tony');
+});
 push.push('tony');
